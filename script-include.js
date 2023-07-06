5ac94f72014544aa9cd49b2a9bcadccd3fdb9f12
Chinnawat Phenwisaphappon_nellika <chinnawat@nellika.co.th>
2023-07-06 21:38:30 +0700

    implement logic for vaildate

--- script-include.js
+++ script-include.js
@@ -2,7 +2,7 @@ const fs = require('fs/promises');
 const xml2js = require('xml2js').parseStringPromise;
 
 async function readXmlFile() {
-    const data = await fs.readFile('./test.xml', 'utf-8');
+    const data = await fs.readFile('./poc-migration/poc-migration.csproj', 'utf-8');
     return data;
 }
 
@@ -12,11 +12,17 @@ async function convertXmlToJson(xml) {
 }
 
 function getCsFiles(json) {
-    const itemCs = json["Project"]["ItemGroup"][1]["Compile"].filter((item) => {{
-        if(!item["$"]["Include"].match(/(.Designer.|.designer.)/))  {
-             return item["$"]["Include"];
-        }
-     }}).map((item) => { return item["$"]["Include"]; });
+    const itemCs = json["Project"]["ItemGroup"][1]["Compile"]
+        .filter((item) => {
+            return !item["$"]["Include"].match(/(.Designer.|.designer.)/) && item["$"]["Include"].startsWith(`Migrations`);
+        })
+        .map((item) => item["$"]["Include"]);
+     return itemCs;
+}
+function getCsDesingFiles(json) {
+    const itemCs = json["Project"]["ItemGroup"][1]["Compile"]
+        .filter((item) => item["$"]["Include"].match(/(.Designer.|.designer.)/)  && item["$"]["Include"].startsWith(`Migrations`))
+        .map((item) => item["$"]["Include"]);
      return itemCs;
 }
 function getEEmbeddedResource(json) {
@@ -30,8 +36,42 @@ async function main() {
     const xml = await readXmlFile();
     const json = await convertXmlToJson(xml);
     const itemCs = getCsFiles(json)
+    const itemDesingCs = getCsDesingFiles(json)
     const embeddedResource = getEEmbeddedResource(json);
-    console.log(embeddedResource);
+    const error = {};
+
+    ignore = ['Migrations\\Configuration.cs'];
+    
+    let notFound = itemCs.some((item) => {
+
+    const [name, surename] = item.split('.');
+        let foundDesing= itemDesingCs.find((itemDesing) => {
+            return itemDesing.toUpperCase() === `${name}.Designer.${surename}`.toUpperCase();
+        })
+
+        let foundEmbedded = embeddedResource.find((itemEmbedded) => {
+            return itemEmbedded.toUpperCase() === `${name}.resx`.toUpperCase();
+        })
+
+        if(foundDesing && foundEmbedded || item.includes(ignore)) {
+            return false;
+        }
+        
+        if(!foundDesing) {
+            error["message"] = `${name}.Designer.${surename} Designer not found`;
+        }
+
+        if(!foundEmbedded) {
+            error["message"] = `${name}.resx Embedded not found`;
+        }
+
+        return true;
+    })
+
+    if(notFound) {
+        console.error(error.message);
+        process.exit(1); 
+    }
 }
 
 main();
